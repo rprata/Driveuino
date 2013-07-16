8529bb922777d5899510b5b2342dc3003a4e352a
Renan Prata <renancprata@gmail.com>
2013-07-16 00:17:33 -0300

    adicionando suporte bluetooth

--- Driveuino/src/main/java/com/rcprata/driveuino/Driveuino.java
+++ Driveuino/src/main/java/com/rcprata/driveuino/Driveuino.java
@@ -1,66 +1,173 @@
 package com.rcprata.driveuino;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.PowerManager;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rcprata.driveuino.core.AccelerometerService;
+import com.rcprata.driveuino.core.BluetoothService;
 import com.rcprata.driveuino.utils.Globals;
+import com.rcprata.driveuino.views.DeviceListActivity;
 
 public class Driveuino extends Activity {
 
-        private TextView axisX, axisY, axisZ;
+    private TextView axisX, axisY, axisZ;
 
-        private AccelerometerService accelerometerService;
+    private PowerManager pm;
+    private PowerManager.WakeLock wl;
 
-        private final Handler handler = new Handler() {
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothService mBluetoothService;
+    private AccelerometerService mAccelerometerService;
 
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case Globals.HANDLER_STATE_ACCELEROMETER:
-                        float[] accelerometerValues = (float[]) msg.obj;
-                        axisX.setText(getString(R.id.axis_x) + accelerometerValues[0]);
-                        axisY.setText(getString(R.id.axis_y) + accelerometerValues[1]);
-                        axisZ.setText(getString(R.id.axis_z) + accelerometerValues[2]);
-                        break;
-                    default:
-                        break;
-                }
+    boolean stateConnection = false;
+
+
+    private final Handler handler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case Globals.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case Globals.STATE_CONNECTED:
+                            stateConnection=true;
+                            break;
+                        case Globals.STATE_CONNECTING:
+                            stateConnection=false;
+                            break;
+                        case Globals.STATE_LISTEN:
+                        case Globals.STATE_NONE:
+                            stateConnection=false;
+                            break;
+                    }
+                    break;
+                case Globals.MESSAGE_READ:
+                    break;
+                case Globals.HANDLER_STATE_ACCELEROMETER:
+                    float[] accelerometerValues = (float[]) msg.obj;
+                    axisX.setText(getString(R.id.axis_x) + accelerometerValues[0]);
+                    axisY.setText(getString(R.id.axis_y) + accelerometerValues[1]);
+                    axisZ.setText(getString(R.id.axis_z) + accelerometerValues[2]);
+                    break;
+                default:
+                    break;
             }
-        };
+        }
+    };
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-            axisX = (TextView)findViewById(R.id.axis_x);
-            axisY = (TextView)findViewById(R.id.axis_y);
-            axisZ = (TextView)findViewById(R.id.axis_z);
+        axisX = (TextView)findViewById(R.id.axis_x);
+        axisY = (TextView)findViewById(R.id.axis_y);
+        axisZ = (TextView)findViewById(R.id.axis_z);
 
-            accelerometerService = new AccelerometerService(this, handler);
+        pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wl = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, Globals.TAG);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, getString(R.string.bt_not_enabled_leaving), Toast.LENGTH_LONG).show();
+            finish();
+            return;
         }
 
-        @Override
-        protected void onResume() {
-            super.onResume();
-            accelerometerService.registerAccelerometerServiceListener();
-        }
+        mAccelerometerService = new AccelerometerService(this, handler);
+    }
 
-        @Override
-        protected void onPause() {
-            super.onPause();
-            accelerometerService.unregisterAccelerometerServiceListener();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, Globals.REQUEST_ENABLE_BT);
+        } else {
+            if(mBluetoothService == null){
+                mBluetoothService = new BluetoothService(this,handler);
+                setupConfigurations();
+            }
         }
+    }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.driveuino, menu);
-            return true;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAccelerometerService.registerAccelerometerServiceListener();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAccelerometerService.unregisterAccelerometerServiceListener();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mBluetoothService!=null) mBluetoothService.stop();
+        mAccelerometerService.unregisterAccelerometerServiceListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.driveuino, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.scan:
+                Intent serverIntent = new Intent(this, DeviceListActivity.class);
+                startActivityForResult(serverIntent, Globals.REQUEST_CONNECT_DEVICE);
+                return true;
+            case R.id.discoverable:
+                ensureDiscoverable();
+                return true;
+            case R.id.info:
+                dialogInflate();
+                return true;
         }
+        return false;
+    }
+
+    private void dialogInflate() {
+        //TODO: Implementar dialog;
+    }
+
+    private void ensureDiscoverable() {
+        Log.d(Globals.TAG, "ensure discoverable");
+        if (mBluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+
+    private void setupConfigurations() {
+
+        if(mBluetoothService==null)
+            mBluetoothService = new BluetoothService(this,handler);
 
     }
+
+}
--- Driveuino/src/main/java/com/rcprata/driveuino/utils/Globals.java
+++ Driveuino/src/main/java/com/rcprata/driveuino/utils/Globals.java
@@ -1,5 +1,7 @@
 package com.rcprata.driveuino.utils;
 
+import java.util.UUID;
+
 /**
  * Created by rcprata on 7/11/13.
  */
@@ -8,6 +10,28 @@ public class Globals {
     public static final String TAG = "Driveuino";
     public static final String VERSION = "0.0.1";
 
-    public static final int HANDLER_STATE_ACCELEROMETER = 1;
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    public static final int HANDLER_STATE_ACCELEROMETER = 6;
+
+
+    //Extras
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+
+    // Intent request codes
+    public static final int REQUEST_CONNECT_DEVICE = 1;
+    public static final int REQUEST_ENABLE_BT = 2;
+
+    //Bluetooth
+    public static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    public static final String NAME = "DriveuinoTalk";
+    public static final int STATE_NONE = 0;       // we're doing nothing
+    public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+    public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
+    public static final int STATE_CONNECTED = 3;  // now connected to a remote device
 
 }
